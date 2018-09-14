d793fce78e53a8d8de829b22a324821334d4acab
rheng001 <rheng001@gmail.com>
2018-09-14 15:33:45 -0700

    changed hash to browser router

--- src/index.js
+++ src/index.js
@@ -1,38 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {HashRouter, Route, Switch} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter, Route, Switch, BrowserRouter } from "react-router-dom";
 
 // Styles
-import '../src/components/NavBar.css';
+import "../src/components/NavBar.css";
 // import '../src/components/HomePage.scss';
-import '../src/components/AboutPage.scss';
-
-
+import "../src/components/AboutPage.scss";
 
 // Import Flag Icons Set
-import 'flag-icon-css/css/flag-icon.min.css';
+import "flag-icon-css/css/flag-icon.min.css";
 // Import Font Awesome Icons Set
-import 'font-awesome/css/font-awesome.min.css';
+import "font-awesome/css/font-awesome.min.css";
 // Import Simple Line Icons Set
-import 'simple-line-icons/css/simple-line-icons.css';
+import "simple-line-icons/css/simple-line-icons.css";
 // Import Main styles for this application
-import '../scss/style.scss'
+import "../scss/style.scss";
 // Temp fix for reactstrap
-import '../scss/core/_dropdown-menu-right.scss'
+import "../scss/core/_dropdown-menu-right.scss";
 
 // Containers
-import Full from './containers/Full/'
-import HomePage from '../src/components/HomePage';
-import AboutPage from '../src/components/AboutPage';
-import ContactPage from '../src/components/ContactPage'
+import Full from "./containers/Full/";
+import HomePage from "../src/components/HomePage";
+import AboutPage from "../src/components/AboutPage";
+import ContactPage from "../src/components/ContactPage";
 
-ReactDOM.render((
-  <HashRouter>
+ReactDOM.render(
+  <BrowserRouter>
     <Switch>
-      <Route path="/about" name="Home" component={AboutPage}/>
-      <Route path="/contact" name="Home" component={ContactPage}/>
-      <Route path="/" name="Home" component={HomePage}/>
-
+      <Route path="/about" name="Home" component={AboutPage} />
+      <Route path="/contact" name="Home" component={ContactPage} />
+      <Route path="/" name="Home" component={HomePage} />
     </Switch>
-  </HashRouter>
-), document.getElementById('root'));
+  </BrowserRouter>,
+  document.getElementById("root")
+);
